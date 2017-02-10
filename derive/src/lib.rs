0a1626337fcd5ad4846866377dc61c86e80d1b2c
Flier Lu <flier.lu@gmail.com>
2017-02-10 23:40:08 +0800

    generate test case to ensure implemented TypeObject

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,14 +1,19 @@
+#![recursion_limit = "128"]
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 #[macro_use]
 extern crate lazy_static;
+extern crate syn;
+#[macro_use]
+extern crate quote;
 extern crate proc_macro;
 extern crate cpython_gen;
 
 use proc_macro::TokenStream;
 
-use cpython_gen::{Builder, format};
+use cpython_gen::format;
 
 lazy_static! {
     static ref LOGGER: () = env_logger::init().unwrap();
@@ -18,7 +23,22 @@ lazy_static! {
 pub fn py_class(input: TokenStream) -> TokenStream {
     let _ = *LOGGER;
 
-    let tokens = Builder::parse(&input.to_string()).unwrap().build();
+    let ast = syn::parse_macro_input(&input.to_string()).unwrap();
+
+    let class_name = ast.ident;
+    let func_name = quote::Ident::new(format!("test_{}", class_name).to_lowercase());
+
+    let tokens = quote!{
+        #[test]
+        fn #func_name() {
+            use cpython::Python;
+
+            let gil = Python::acquire_gil();
+            let py = gil.python();
+
+            assert!(!py.get_type::<#class_name>().as_type_ptr().is_null());
+        }
+    };
 
     debug!("generated code\n{}", format::code(tokens.as_str()));
 
